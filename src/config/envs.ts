35cd8bc4b0548f0c4dc93cd197cbc227224afb8e
argalarza <andyricardog234@gmail.com>
2025-02-14 00:08:38 -0500

    🚀 Implementing MongoDB 🐱💾 (replacing SQLite)

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -4,34 +4,25 @@ import * as joi from 'joi';
 interface EnvVars {
   PORT: number;
   DATABASE_URL: string;
-
-  NATS_SERVERS: string[];
 }
 
 const envsSchema = joi.object({
   PORT: joi.number().required(),
   DATABASE_URL: joi.string().required(),
-
-  NATS_SERVERS: joi.array().items( joi.string() ).required(),
 })
-.unknown(true);
+  .unknown(true);
 
 const { error, value } = envsSchema.validate({ 
-  ...process.env,
-  NATS_SERVERS: process.env.NATS_SERVERS?.split(',')
+  ...process.env
 });
 
-
-if ( error ) {
-  throw new Error(`Config validation error: ${ error.message }`);
+if (error) {
+  throw new Error(`Config validation error: ${error.message}`);
 }
 
-const envVars:EnvVars = value;
-
+const envVars: EnvVars = value;
 
 export const envs = {
   port: envVars.PORT,
   databaseUrl: envVars.DATABASE_URL,
-
-  natsServers: envVars.NATS_SERVERS,
-}
+};
--- src/main.ts
+++ src/main.ts
@@ -2,21 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { envs } from './config';
-import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
 
   const logger = new Logger('Main');
 
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
-    AppModule,
-    {
-      transport: Transport.NATS,
-      options: {
-        servers: envs.natsServers
-      }
-    }
-  );
+  const app = await NestFactory.create(AppModule); // Removed Microservice configuration
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -25,8 +16,7 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen();
-  logger.log(`Products Microservice running on port ${ envs.port }`);
-
+  await app.listen(envs.port);
+  logger.log(`Products Microservice running on port ${envs.port}`);
 }
 bootstrap();
